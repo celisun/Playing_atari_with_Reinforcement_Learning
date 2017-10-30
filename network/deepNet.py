7caa6104edbd51273cdb62b766f56ab5fd60bb18
celisun <s1676438@sms.ed.ac.uk>
2017-10-30 16:21:12 -0400

    work

--- network/deepNet.py
+++ network/deepNet.py
@@ -1,30 +1,28 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class net(nn.Module):
+class Net(nn.Module):
     """
         Simple multiple layer net, with 5 hidden layers
-        each equipped with rectified non-linearity.
+        each equipped with a rectified non-linearity.
         
     """
     
     def __init__ (self, input_dim, output_dim, hidden_dim):
-        super(net, self).__init__()
+        super(Net, self).__init__()
         self.input_layer = nn.Linear(input_dim, hidden_dim)
         self.hidden_layer_1 = nn.Linear(hidden_dim, hidden_dim)
         self.hidden_layer_2 = nn.Linear(hidden_dim, hidden_dim)
         self.hidden_layer_3 = nn.Linear(hidden_dim, hidden_dim)
         self.hidden_layer_4 = nn.Linear(hidden_dim, hidden_dim)
-        self.hidden_layer_5 = nn.Linear(hidden_dim, hidden_dim)
-        
+        self.hidden_layer_5 = nn.Linear(hidden_dim, hidden_dim)     
         self.output_layer = nn.Linear(hidden_dim, output_dim)
-        self.input_dim = input_dim
+
                    
         
     def forward(self, x):
-        print str(x.data.size())
+        #print str(x.data.size())
         x = self.input_layer(x)
-        print str(x.data.size())
         x = F.relu(x)
         
         x = F.relu(self.hidden_layer_1(x))
