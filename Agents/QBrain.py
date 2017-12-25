77244feb75b6bff4b01b128f9f49f2b898959622
celishxxp <sunceli@brandeis.edu>
2017-12-25 11:58:39 -0500

    clean up

--- Agents/QBrain.py
+++ Agents/QBrain.py
@@ -26,7 +26,6 @@ from network.deepnet import Net
 
 
 
- 
 # if gpu is to be used
 use_cuda = torch.cuda.is_available()
 FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
@@ -42,11 +41,6 @@ config_net = config_net
 # named tuple representing a single transition in enviornment
 Transition = namedtuple('Transition',
                         ('state', 'action', 'reward', 'next_state'))
-
-
-
-
-
 
 
 class QLearningAgent(object):
@@ -70,7 +64,6 @@ class QLearningAgent(object):
         self.replay_memory = ReplayMemory(replay_memory_capacity if replay_memory_capacity is not None else config["replay_memory_capacity"])
 
         #// initialize Q network
-
         if self.learnable is not None:
             self.q = CNN(config["frame_size_h"], config["frame_size_w"], \
                          config["action_list_n"]) if self.learnable=='CNN' \
@@ -94,8 +87,6 @@ class QLearningAgent(object):
     def act_random(self):
         action = self.action_space.sample()
         return action, 0    
-
-
       
     def act_learn(self, observation, eps=None):
         """
@@ -117,29 +108,22 @@ class QLearningAgent(object):
         observation = observation.view(config_net["inputs_dim"]) if self.learnable=='CNN' \
                      else observation.view(1, config_net["inputs_dim"])
         qvalue_list = self.q(Variable(observation, volatile=True).type(FloatTensor)) 
-        
-       
-        
+               
+        # Epsilon greedy control 
         if np.random.random() > eps:
             qvalue, action = torch.max(qvalue_list.data, 1)
             action = action.numpy()[0][0]   #from tensor 
-            print "========= Q ACTION PICKING "
-            print "Q(s) to pick from: "+str(qvalue_list)
-            print "Agent pick the best one: " + str(action)
-            print "of Q-value: "+ str(qvalue)
-            print "========= \n"
         else: 
             qvalue, _= torch.max(qvalue_list.data, 1)
             action, _= self.act_random()
         
         qvalue = qvalue.numpy()[0][0]
         return action, qvalue           # always return MAX Q possible in current prediction 11/9
-
+       
     def train(self, env):
         self.epoch_done=0
         t_num=0
-        for i in range(config["n_itr"]):
-            
+        for i in range(config["n_itr"]): 
             print('')
             print(' =============================> Epoch ' + str(self.epoch_done))
 
@@ -150,17 +134,15 @@ class QLearningAgent(object):
             loss_data_=0.
             reward_total=0.     # for lunar land
             
-
             for t in count():
                 print(' episode: ' + str(t+1))
-                
-                # Select and perform the action
+                # Select and perform action
                 action, qvalue = self.act_random() if self.q is None \
                                     else self.act_learn(torch.from_numpy(obs))
                 obs2, reward, done, info = env.step(action)
                 obs2 = self.obFixer(self.observation_space, obs2)
                 
-                 
+                # convert reward to -1, 0 or 1
                 if done: 
                     obs2 = None  
                 _reward_ =0.
@@ -169,17 +151,14 @@ class QLearningAgent(object):
                 elif reward<0:
                     _reward_ = -1
 
-                
-                ## I'm guaranteeing half of good transitions  
+                ## I'm guaranteeing half of good transitions(r>0) in memory  
                 if (t_num%2==0 and reward >0) or (t_num%2!=0 and reward<0):
                     self.replay_memory.save(obs, action, _reward_, obs2)
                     t_num += 1
-                          
-                
+                                     
                 # Perform on step of optimization to Q net
                 if self.q is not None:  
                     loss_data_ = self.optimize_model()
-
                     
                 reward_total += reward
                 qvalue_total += qvalue
@@ -194,9 +173,7 @@ class QLearningAgent(object):
                     self.average_total_Q.append(qvalue_total/(t+1))
                     self.average_loss.append(loss_total/(t+1))
                     self.epoch_durations.append(t+1)
-                    break
-
-
+                    break                 
             self.epoch_done += 1
 
         print('Complete')
@@ -204,9 +181,7 @@ class QLearningAgent(object):
         env.render(close=True)
         env.close()
 
-
     def optimize_model(self):
-
         BATCH_SIZE = config["BATCH_SIZE"]
         ACTION_LIST = config["action_list_n"]
         INPUT_SIZE = config_net["inputs_dim"]
@@ -217,8 +192,7 @@ class QLearningAgent(object):
 
         batch = Transition(*zip(*transitions))
         print ('sample batch from memory...\n')
-        
-              
+           
         # Concatenate the batch elements of each
         # state, action , reward in every transition
         state_batch = Variable(torch.from_numpy(np.asarray(batch.state))).float()    ##.double()
@@ -228,80 +202,40 @@ class QLearningAgent(object):
         self.optimizer.zero_grad()
         
         # Compute Q(s_t, a) by model
-        # the model computes Q(s_t) and we select the columns
-        # of actions taken in record
-        state_values_ = self.q(\
-                    state_batch if self.learnable=='CNN' else state_batch.view(-1,INPUT_SIZE)\
-                    )
-        ##############
-        #print (" V(s_{t}, a): "+ str(state_values_))
-        ##############
-        ##############
-        #print ("action batch: "+ str(action_batch))
-        ##############
-        
+        state_values_ = self.q(state_batch if self.learnable=='CNN' else state_batch.view(-1,INPUT_SIZE))
 
+        # Select the columns in Q for actions taken  
         state_action_values = state_values_[0].view(1,ACTION_LIST).index_select(1, action_batch[0].long())
         for i in range(len(action_batch)-1):
             state_action_values = torch.cat((state_action_values, \
                                     state_values_[i+1].view(1,ACTION_LIST).index_select(1, action_batch[i+1].long())),0)
         state_action_values = state_action_values.index_select(1, Variable(torch.LongTensor([0])))
-         
-        
-        ##############
-        print (" Q(s_{t}, a): "+ str(state_action_values))
-        ##############
-
-        
-        non_final_mask = ByteTensor(tuple(map(lambda s: s is not None,\
-                                             batch.next_state)))
+     
+        non_final_mask = ByteTensor(tuple(map(lambda s: s is not None, batch.next_state)))
         # We don't want backprop expected action values, volatile shuts down requires-grad
         non_final_next_states = Variable (torch.cat([torch.from_numpy(x) \
                                                      for x in np.asarray(batch.state) \
                                            if x is not None]), volatile=True).float()        ##.double()
            
-        # Compute state value V(s_{t+1}) by model for all next states,
-        # then select the maxQ value of each:
-        ## MAX(a)Q(s_{t+1}, a)
+        # Compute state value V(s_{t+1}) by model  
+        # then select the maxQ value of each: MAX(a)Q(s_{t+1}, a)
         next_state_values = Variable(torch.zeros(BATCH_SIZE).type(Tensor)).float()       ##.double()
         next_state_values[non_final_mask], _ = torch.max( self.q( \
             non_final_next_states if self.learnable=='CNN' \
-                    else non_final_next_states.view(-1,INPUT_SIZE) \
-            ), 1)   
+                    else non_final_next_states.view(-1,INPUT_SIZE)), 1)   
         
-        
-        
-        #############
-        print("V(s_{t+1}): " + str(next_state_values))
-        ############
-
         # Compute the expected Q values
-        ## Bellman equation
-        ## Q_{t+1}(s,a) = r + gamma * MAX(a')Q(s', a')
-        expected_state_action_values =  reward_batch + (next_state_values * config["GAMMA"]) 
-        
-        #############
-        print("r: "+str(reward_batch))
-        print("y_{t}: " + str(expected_state_action_values))
-        ############
-        
+        expected_state_action_values =  reward_batch + (next_state_values * config["GAMMA"])       # Q_{t+1}(s,a) = r + gamma * MAX(a')Q(s', a')
 
         # Compute Mean-Square Error loss from Q and expected Q*
-        ## loss(x, y) = 1/n sum|x-y|^2
-        loss = self.loss(state_action_values, expected_state_action_values)
+        loss = self.loss(state_action_values, expected_state_action_values)   # loss(x, y) = 1/n sum|x-y|^2
         
-        ##############
-        #print("LOSS: " + str( loss.data.numpy()[0]))
-        ################
-        
-         
+        # Update model 
         loss.backward()
         self.optimizer.step()
         
         return loss.data.numpy()[0]
-
-    
-    
+   
     def plot_durations():
         fig = plt.figure(figsize=(14,14))
         plt.clf()
