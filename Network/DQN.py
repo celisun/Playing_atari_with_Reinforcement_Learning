c0bc43edcdca577d622bf5588e43158b5855f11d
celishxxp <sunceli@brandeis.edu>
2018-01-05 14:53:57 -0500

    Update DQN.py

--- Network/DQN.py
+++ Network/DQN.py
@@ -25,6 +25,7 @@ class DQN(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
+        
         return self.linear(x.view(x.size(0), -1))
 
     
